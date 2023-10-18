b523a33b555deb05f41330c1bc2baeda74a51121
Diego Escarcega <143562608+clancytheman@users.noreply.github.com>
2023-10-18 17:50:14 -0600

    Add files via upload

---
+++ EVA2_11_CADENAS/src/main/java/com/mycompany/eva2_11_cadenas/EVA2_11_CADENAS.java
@@ -0,0 +1,16 @@
+package com.mycompany.eva2_11_cadenas;
+
+/**
+ *
+ * @author DIEGO ESCARCEGA
+ */
+public class EVA2_11_CADENAS {
+
+    public static void main(String[] args) {
+        String CHAIN = "HELLO";
+         if (CHAIN.equals("HELLO"))
+              System.out.println("CHAINS ARE THE SAME");
+         else
+              System.out.println("CHAINS ARE NOT THE SAME");
+    }
+}
